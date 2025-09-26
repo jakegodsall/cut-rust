f58ab90600bf9ead960959ee4e5beecca0568767
Jake Godsall <jake@cloudconstruct.co.uk>
2025-09-26 15:44:15 +0100

    read file to the console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+
+
+enum Mode { Byte, Character, Field }
+
+fn read_file(file_handle: &str) -> Result<String, Box<std::io::Error>> {
+    let mut file = std::fs::File::open(file_handle)?;
+    let mut content = String::default();
+    file.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let content = read_file("data/fourchords.csv")?;
+
+    println!("{}", content);
+
+    Ok(())
 }
